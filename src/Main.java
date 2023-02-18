2b65a50c717bc37939052b0212edd2171bc1fbeb
Nathalia Aristigueta <aristiguetanathaliargmail.com>
2023-02-18 10:04:15 +0100

    Hemos commiteado por primera vez todos los archivos

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    int value_int = 20;
+        System.out.println(value_int);
+
+        String value_string = "Java";
+        System.out.println(value_string);
+
+        double value_double = 23.63d;
+        System.out.println(value_double);
+        
+
+    }
+
+}
